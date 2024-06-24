948eee61a0bac989ee4089d37392c505fee7a97e
Doma <brady.gills@gmail.com>
2024-06-24 01:06:11 -0700

    Full basic python file install example
    Basic working example of a python script -> exe -> standalone installer

--- AlternativeDesktop.py
+++ AlternativeDesktop.py
@@ -1,2 +1,23 @@
-greeting = "hello"
-print(greeting)
+import subprocess
+import time
+import pyautogui
+import os
+
+def launch_notepad():
+    # Launch Notepad
+    notepad_process = subprocess.Popen(['notepad.exe'])
+
+    # Wait for Notepad to open
+    time.sleep(2)
+
+    # Type "Hello, World!"
+    pyautogui.typewrite('Hello, World!', interval=0.1)
+
+    # Wait for 5 seconds
+    time.sleep(5)
+
+    # Terminate Notepad
+    notepad_process.terminate()
+
+if __name__ == '__main__':
+    launch_notepad()
