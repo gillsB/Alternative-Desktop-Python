8a141d1424c3ef485a1cf181d50051635b144c41
Doma <brady.gills@gmail.com>
2024-06-29 23:29:17 -0700

    Get Set and Save Settings
    get_Setting(key)
    set_setting(key, value)
    save_setting(settings)       (called from set_setting)
    set_dir(directory for settings.json) MUST BE CALLED

--- AlternativeDesktop.py
+++ AlternativeDesktop.py
@@ -8,7 +8,7 @@ import shutil
 import zipfile
 import json
 from updater import check_for_updates
-from settings import load_settings
+from settings import load_settings, set_dir, get_setting, set_setting
 from PyQt5.QtWidgets import QApplication
 import keyboard
 from desktop import main as desktop_main
@@ -19,9 +19,19 @@ RELEASES_URL = f"https://api.github.com/repos/{GITHUB_REPO}/releases/latest"
 SETTINGS_FILE = "config/settings.json"
 
 if __name__ == '__main__':
+    set_dir(SETTINGS_FILE)
     settings = load_settings(SETTINGS_FILE)
     print(settings)
     if settings.get("update_on_launch", True):
         check_for_updates(CURRENT_VERSION, GITHUB_REPO, RELEASES_URL)
+    
+    value = get_setting("update_on_launch")
+    print(value)
+    set_setting("update_on_launch", False)
+    value = get_setting("update_on_launch")
+    print(value)
+    set_setting("update_on_launch", True)
+    value = get_setting("update_on_launch")
+    print(value)
     
     desktop_main()
--- settings.py
+++ settings.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+DIRECTORY = None
+
 def load_settings(SETTINGS_FILE):
     if os.path.exists(SETTINGS_FILE):
         with open(SETTINGS_FILE, "r") as f:
@@ -8,3 +10,23 @@ def load_settings(SETTINGS_FILE):
     else:
         print("Error loading settings, expected file at: " + SETTINGS_FILE )
         return {}
+
+def get_setting(key, default=None):
+    settings = load_settings(DIRECTORY)
+    return settings.get(key, default)
+
+def set_setting(key, value):
+    if DIRECTORY != None:
+        settings = load_settings(DIRECTORY)
+        settings[key] = value
+        save_settings(settings)
+
+def save_settings(settings):
+    with open(DIRECTORY, "w") as f:
+        json.dump(settings, f, indent=4)
+
+
+
+def set_dir(directory):
+    global DIRECTORY
+    DIRECTORY = directory
