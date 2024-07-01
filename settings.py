36219ca5e0ed65006df835d9dec0457efc9d4c9d
Doma <brady.gills@gmail.com>
2024-06-30 22:53:17 -0700

    Settings.json now builds from scratch
    Settings.json builds itself from scratch with a default settings located in settings.py. It will also check for new settings and add them if they are missing (allows updating and adding settings without distributing a default .json where you have to change your settings every update).

--- settings.py
+++ settings.py
@@ -1,7 +1,12 @@
 import json
 import os
+from pathlib import Path
 
 DIRECTORY = None
+DEFUALT_SETTINGS = {
+        "update_on_launch": True,
+        "desktop_keybind": "<alt>+d"
+}
 
 def load_settings(SETTINGS_FILE):
     if os.path.exists(SETTINGS_FILE):
@@ -9,6 +14,14 @@ def load_settings(SETTINGS_FILE):
             return json.load(f)
     else:
         print("Error loading settings, expected file at: " + SETTINGS_FILE )
+        return {}
+    
+def load_local():
+    if os.path.exists(DIRECTORY):
+        with open(DIRECTORY, "r") as f:
+            return json.load(f)
+    else:
+        print("Error loading settings, expected file at: " + DIRECTORY )
         return {}
 
 def get_setting(key, default=None):
@@ -30,3 +43,21 @@ def save_settings(settings):
 def set_dir(directory):
     global DIRECTORY
     DIRECTORY = directory
+    if os.path.exists(DIRECTORY):
+        check_for_new_settings()
+    else:
+        build_settings()
+
+
+def build_settings():
+    save_settings(DEFUALT_SETTINGS)
+
+def check_for_new_settings():
+    settings = load_local()
+    new_settings = False
+    for key, value in DEFUALT_SETTINGS.items():
+        if key not in settings:
+            settings[key] = value
+            new_settings = True
+    if new_settings:
+        save_settings(settings)
