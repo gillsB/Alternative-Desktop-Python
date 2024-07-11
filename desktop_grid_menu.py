bc64a4b6330a01552ac7dfa1daf8e453559fe56c
Doma <brady.gills@gmail.com>
2024-07-10 23:09:13 -0700

    Simplified to QFormLayout
    simplified it to match settings menu's design. simpler, cleaner....

--- desktop_grid_menu.py
+++ desktop_grid_menu.py
@@ -6,57 +6,29 @@ class Menu(QDialog):
 
         self.setWindowTitle("Settings")
         layout = QFormLayout()
-
+        self.setLayout(layout)
         self.setWindowTitle("QLabel and QLineEdit")
-        self.setFixedSize(300, 200)
 
-        name = QLabel("Name : ")
         self.name_le = QLineEdit()
-
-        icon_path = QLabel("Icon Path : ")
         self.icon_path_le = QLineEdit()
-
-        exec_path = QLabel("Executable Path: ")
         self.exec_path_le = QLineEdit()
         
         
-        
-        
-        '''
-        self.line_edit.textChanged.connect(self.text_changed)
-        self.line_edit.cursorPositionChanged.connect(self.cursor_position_changed)
-
-        self.line_edit.returnPressed.connect(self.return_pressed)
-
-        self.line_edit.selectionChanged.connect(self.selection_changed)
-
-        self.line_edit.textEdited.connect(self.text_edited)
-
-        '''
 
         button = QPushButton("save")
         button.clicked.connect(self.button_clicked)
-        
 
-        name_layout = QHBoxLayout()
-        name_layout.addWidget(name)
-        name_layout.addWidget(self.name_le)
+        layout.addRow("Name: ", self.name_le)
+        layout.addRow("Icon Path: ", self.icon_path_le)
+        layout.addRow("Executable Path: ", self.exec_path_le)
 
-        icon_path_layout = QHBoxLayout()
-        icon_path_layout.addWidget(icon_path)
-        icon_path_layout.addWidget(self.icon_path_le)
+        save_button = QPushButton("Save")
+        #save_button.clicked.connect(self.save_settings)
+        layout.addWidget(save_button)
 
-        exec_path_layout = QHBoxLayout()
-        exec_path_layout.addWidget(exec_path)
-        exec_path_layout.addWidget(self.exec_path_le)
 
-        v_layout = QVBoxLayout()
-        v_layout.addLayout(name_layout)
-        v_layout.addLayout(icon_path_layout)
-        v_layout.addLayout(exec_path_layout)
-        v_layout.addWidget(button)
 
-        self.setLayout(v_layout)
+
 
 
     def button_clicked(self):
