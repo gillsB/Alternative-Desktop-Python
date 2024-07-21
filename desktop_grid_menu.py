748f9ecadf4e60d7c15e8d1b67bbd3c2dd2a1bfc
Doma <brady.gills@gmail.com>
2024-07-20 22:10:28 -0700

    Drag and drop only changes name if name is empty
    To avoid changing the name just by drag and dropping an exec_path or an image icon.
    
    For example before if you dragged an .exe called Application.exe it would name it Application.exe and then dragged an image called Image.png into the window it would change the name of the desktop_icon to Image.png or vice versa.

--- desktop_grid_menu.py
+++ desktop_grid_menu.py
@@ -203,12 +203,13 @@ class Menu(QDialog):
                 if file_extension in image_extensions:
                     if file_extension == 'ico':
                         file_path = self.upscale_ico(file_path)
-                        self.name_le.setText(file_name)
                     else:
                         self.icon_path_le.setText(file_path)
-                        self.name_le.setText(file_name)
                 else:
-                    self.exec_path_le.setText(file_path)
+                    self.exec_path_le.setText(file_path)           
+
+                #only change name line edit if the name is empty (i.e. take the name of the first drag and drop or do not overwrite a name already existing)
+                if self.name_le.text == "":
                     self.name_le.setText(file_name)
                 event.acceptProposedAction()
     
