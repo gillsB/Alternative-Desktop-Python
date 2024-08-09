274b0fbcf98413b0bf82aea7cf9cddbae3cf4954
Doma <brady.gills@gmail.com>
2024-08-08 22:38:46 -0700

    Icon selection now supports >2 found icons
    Icon_selection.py menu now supports 2+ found icons. Before it was hard coded just to sort between 2 fixed icons. Now that the possible maximum is 3 found icons the icon_selection now creates unlimited labels to choose from (as many as found).
    
    Removed the website link auto image gen taking max priority which was added just to test it. Now sets it as icon if no executable or specified icon path. And treats it the same as any other found icon. (icon_selection menu called if > 1 found icon.)

--- thumbnail_gen/icon_selection.py
+++ thumbnail_gen/icon_selection.py
@@ -14,10 +14,10 @@ class ClickableLabel(QLabel):
             self.radio_button.clicked.emit()
 
 class IconSelectionDialog(QDialog):
-    def __init__(self, icon1_path, icon2_path):
+    def __init__(self, icon_paths):
         super().__init__()
         self.setWindowTitle("Select an Icon")
-        self.selected_icon = icon1_path  # Default to the first icon
+        self.selected_icon = None 
         
         # Main layout
         main_layout = QVBoxLayout()
@@ -31,38 +31,28 @@ class IconSelectionDialog(QDialog):
         
         # Button group for radio buttons
         self.button_group = QButtonGroup(self)
-
-        print(f"ICON1 PATH = {icon1_path}")
-        print(f"ICON2 PATH = {icon2_path}")
         
-        # Icon 1
-        icon1_layout = QVBoxLayout()
-        icon1_pixmap = QPixmap(icon1_path)
-        icon1_label = ClickableLabel()
-        icon1_label.setPixmap(icon1_pixmap)
-        icon1_radio = QRadioButton("Option 1")
-        icon1_radio.setChecked(True)
-        icon1_radio.clicked.connect(lambda: self.select_icon(icon1_path))
-        icon1_label.radio_button = icon1_radio
-        self.button_group.addButton(icon1_radio)
-        icon1_layout.addWidget(icon1_label)
-        icon1_layout.addSpacerItem(QSpacerItem(0, 10, QSizePolicy.Minimum, QSizePolicy.Fixed))
-        icon1_layout.addWidget(icon1_radio, alignment=Qt.AlignHCenter)
-        icons_layout.addLayout(icon1_layout)
-        
-        # Icon 2
-        icon2_layout = QVBoxLayout()
-        icon2_pixmap = QPixmap(icon2_path)
-        icon2_label = ClickableLabel()
-        icon2_label.setPixmap(icon2_pixmap)
-        icon2_radio = QRadioButton("Option 2")
-        icon2_radio.clicked.connect(lambda: self.select_icon(icon2_path))
-        icon2_label.radio_button = icon2_radio
-        self.button_group.addButton(icon2_radio)
-        icon2_layout.addWidget(icon2_label)
-        icon2_layout.addSpacerItem(QSpacerItem(0, 10, QSizePolicy.Minimum, QSizePolicy.Fixed))
-        icon2_layout.addWidget(icon2_radio, alignment=Qt.AlignHCenter)
-        icons_layout.addLayout(icon2_layout)
+        # Create labels and radio buttons for each icon path
+        for index, icon_path in enumerate(icon_paths):
+            if icon_path:
+                icon_layout = QVBoxLayout()
+                icon_pixmap = QPixmap(icon_path)
+                icon_label = ClickableLabel()
+                icon_label.setPixmap(icon_pixmap)
+                icon_radio = QRadioButton(f"Option {index + 1}")
+                
+                # Set default selected icon
+                if index == 0:
+                    icon_radio.setChecked(True)
+                    self.selected_icon = icon_path
+                
+                icon_radio.clicked.connect(lambda _, path=icon_path: self.select_icon(path))
+                icon_label.radio_button = icon_radio
+                self.button_group.addButton(icon_radio)
+                icon_layout.addWidget(icon_label)
+                icon_layout.addSpacerItem(QSpacerItem(0, 10, QSizePolicy.Minimum, QSizePolicy.Fixed))
+                icon_layout.addWidget(icon_radio, alignment=Qt.AlignHCenter)
+                icons_layout.addLayout(icon_layout)
         
         main_layout.addLayout(icons_layout)
         
@@ -79,8 +69,9 @@ class IconSelectionDialog(QDialog):
     def select_icon(self, icon_path):
         self.selected_icon = icon_path
 
-def select_icon_from_paths(icon1_path, icon2_path):
-    dialog = IconSelectionDialog(icon1_path, icon2_path)
+def select_icon_from_paths(*icon_paths):
+    dialog = IconSelectionDialog([path for path in icon_paths if path])
     if dialog.exec() == QDialog.Accepted:
         return dialog.selected_icon
     return None
+
