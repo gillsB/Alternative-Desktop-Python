e29eeaee29e470865a0b8f9abe0460f54d429dd9
Doma <brady.gills@gmail.com>
2024-08-11 22:52:45 -0700

    favicon get now directly based on google link api
    Removed a lengthy requests.get() sequence for getting an icon from a website.
    For most sites it was the same as if not slightly slower and lower quality than the google api (simply a get request to the website: "https://www.google.com/s2/favicons?domain={url}&sz={ICON_SIZE}". And at the same time trying to get it from a website that is not responding (no responses or just bad website name etc.) would cause a very long pause in the program while it awaited the server to respond, up to 15 seconds to reach max retries.
    I have instead just focused on believing that the google api will generally work in almost all cases (even works on some obscure websites I have tried), and just default down to browser icon should it not be found through the google API.

--- thumbnail_gen/favicon_to_image.py
+++ thumbnail_gen/favicon_to_image.py
@@ -1,44 +1,21 @@
 import os
 import requests
 from PIL import Image
-from bs4 import BeautifulSoup
 
 ICON_SIZE = 128
 
 def favicon_to_image(url, output_path):
+
+    # attempt to get favicon from google favicon service
+    google_api = f"https://www.google.com/s2/favicons?domain={url}&sz={ICON_SIZE}"
     try:
-        response = requests.get(url, timeout=5)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        
-
-        # attempt to get favicon from google favicon service
-        google_api = f"https://www.google.com/s2/favicons?domain={url}&sz={ICON_SIZE}"
-        try:
-            output_path = os.path.join(output_path, "icon3.png")
-            saved_path = save_favicon(google_api, output_path)
-            return saved_path
-        except requests.RequestException as e:
-            print(f"Failed to fetch Google favicon: {e}")
-
-
-        # Search for Apple Touch Icon or larger icon links on website
-        icon_link = (
-            soup.find("link", rel="apple-touch-icon") or
-            soup.find("link", rel="apple-touch-icon-precomposed") or
-            soup.find("link", rel="icon") or
-            soup.find("link", rel="shortcut icon")
-        )
-        if icon_link:
-            icon_url = icon_link.get("href")
-            if not icon_url.startswith("http"):
-                icon_url = url.rstrip("/") + "/" + icon_url.lstrip("/")
-            saved_path = save_favicon(icon_url, output_path)
-            return saved_path
-
-
+        output_path = os.path.join(output_path, "icon3.png")
+        saved_path = save_favicon(google_api, output_path)
+        return saved_path
     except requests.RequestException as e:
-        print(f"Failed to fetch favicon: {e}")
-        return None
+        print(f"Failed to fetch Google favicon: {e}")
+
+
     
 
 def save_favicon(favicon_url, save_path):
