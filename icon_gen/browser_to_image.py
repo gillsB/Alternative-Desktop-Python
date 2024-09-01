b5c7c78ac6f2e6bac90d8b484c91a0f1ee22e413
gillsB <78826213+gillsB@users.noreply.github.com>
2024-08-31 21:00:01 -0700

    Updated browser to image to use exe_to_image.
    Basically uses the same function re-written anyways.
    
    Also added logging to exe_to_image.py

--- icon_gen/browser_to_image.py
+++ icon_gen/browser_to_image.py
@@ -2,12 +2,14 @@ import winreg
 import os
 from PIL import Image
 from icoextract import IconExtractor, IconExtractorError
+import logging
+from icon_gen.exe_to_image import exe_to_image
 
-ICON_SIZE = 0
+logger = logging.getLogger(__name__)
+
 
 def browser_to_image(output_path, icon_size):
-    global ICON_SIZE
-    ICON_SIZE = icon_size
+    logger.info("Attempting to get default web browser")
     with winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice") as key:
         prog_id = winreg.QueryValueEx(key, 'ProgId')[0]
     
@@ -15,33 +17,8 @@ def browser_to_image(output_path, icon_size):
         command = winreg.QueryValueEx(key, None)[0]
     
     path = command.split('"')[1]
-    return get_icon(path, output_path, icon_size)
-
-def get_icon(path, output_path):
-    try:
-        extractor = IconExtractor(path)
-
-        output_path = os.path.join(output_path, "icon3.png")
-
-        # Export the first group icon to a .ico file
-        extractor.export_icon(output_path, num=0)
-
-        # Or read the .ico into a buffer, to pass it into other code
-        data = extractor.get_icon(num=0)
-
-        #from PIL import Image
-        im = Image.open(data)
-        img_resized = im.resize((ICON_SIZE, ICON_SIZE), Image.Resampling.LANCZOS)
-
-        # Save the resized image to the output path
-        img_resized.save(output_path)
-        # ... manipulate a copy of the icon
-
-        return output_path
-
-    except IconExtractorError:
-        # No icons available, or the icon resource is malformed
-        pass
+    logger.info(f"Path for default browser = {path}")
+    return exe_to_image(path, output_path, icon_size)
 
 
 
--- icon_gen/exe_to_image.py
+++ icon_gen/exe_to_image.py
@@ -2,33 +2,44 @@ from icoextract import IconExtractor, IconExtractorError
 from PIL import Image
 import os
 from icon_gen.extract_ico_file import extract_ico_file
+import logging
 
 
+logger = logging.getLogger(__name__)
 
 def exe_to_image(lnk_path, output_path, icon_size):
 
 
     try:
+        logger.info("Attempting to extract the icon from the path")
         extractor = IconExtractor(lnk_path)
+        logger.info("Extraction successful")
+
 
         output_path = os.path.join(output_path, "icon2.png")
 
         # Export the first group icon to a .ico file
-        extractor.export_icon(output_path, num=0)
+        #extractor.export_icon(output_path, num=0)
 
         # Or read the .ico into a buffer, to pass it into other code
         data = extractor.get_icon(num=0)
+        logger.info("Extracted icon loaded into buffer.")
 
         #from PIL import Image
         im = Image.open(data)
         img_resized = im.resize((icon_size, icon_size), Image.Resampling.LANCZOS)
+        logger.info("Resized image (still in buffer).")
 
         # Save the resized image to the output path
         img_resized.save(output_path)
-        # ... manipulate a copy of the icon
+        logger.info(f"Icon saved to path = {output_path}, now returning this path.")
 
         return output_path
 
-    except IconExtractorError:
+    except IconExtractorError as e:
         # No icons available, or the icon resource is malformed
+        logger.error(f"IconExtractorError occurred extracting icon: {e}")
+        pass
+    except Exception as e:
+        logger.error(f"Other error occurred: {e}")
         pass
