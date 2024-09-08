9664197dd470bd722678d71344ea9df41765cfe4
gillsB <78826213+gillsB@users.noreply.github.com>
2024-09-08 02:09:13 -0700

    Complete "On Closing the program" setting
    Completed "On closing the program" setting with 2 options:
    
    "Minimize to tray" (default, when closing through the X or right click close it will minimize to tray and still respond to keybind restore or right click restore.)
    
    "Terminate the program" (When you close through any means immediately terminate the program).

--- desktop.py
+++ desktop.py
@@ -77,20 +77,24 @@ class OverlayWidget(QWidget):
         
     # Override base CloseEvent to just hide it. (Tray item already exists)
     def closeEvent(self, event):
-        event.ignore()
-        self.restored_window = True
-        if self.isMinimized:
-            # Showing these as Maximized/Normal is important for actually bringing them back up in a single toggle_window_state() call.
-            # Otherwise it would take multiple calls or bring it up in the background (non focused).
-            if self.is_maximized == True:
-                logger.info("Quickly showing window as Maximized before hiding")
-                self.showMaximized()
-            else:
-                logger.info("Quickly showing window as normal before closing")
-                self.showNormal()
-        
+        if get_setting("on_close", 0) == 1:
+            logger.info("Program closing by closeEvent with on_close set to 'Terminiate the program' on close.")
+            super(OverlayWidget, self).closeEvent(event)
+        else:
+            event.ignore()
+            self.restored_window = True
+            if self.isMinimized:
+                # Showing these as Maximized/Normal is important for actually bringing them back up in a single toggle_window_state() call.
+                # Otherwise it would take multiple calls or bring it up in the background (non focused).
+                if self.is_maximized == True:
+                    logger.info("Quickly showing window as Maximized before hiding")
+                    self.showMaximized()
+                else:
+                    logger.info("Quickly showing window as normal before closing")
+                    self.showNormal()
+            
 
-        self.hide()
+            self.hide()
 
     # This is only the tray icon -> Restore menu. Running self.toggle_window_state() makes it the same as keybind restore.
     # Or can always override this say for instance if you want it to always restore to maximized.
@@ -174,7 +178,6 @@ class OverlayWidget(QWidget):
     def show_settings(self):
         dialog = SettingsDialog(parent=self)
         dialog.exec()
-        print(self.windowState())
     
     def change_opacity(self ,i):
         logger.info(f"Change opacity = {float(i/100)}")
