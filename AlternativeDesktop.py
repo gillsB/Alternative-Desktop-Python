adbc798525ef93fd19fbb705ed147feec852db75
gillsB <78826213+gillsB@users.noreply.github.com>
2024-09-22 20:09:06 -0700

    V0.1.002
    Small release to fix some things like patch notes being unintentionally left out from the previous release.
    See release for further patch notes.
    
    I do plan on releasing more often (was a month between V0.1.000 and V0.1.001) but more like ~weekly updates unless something important to address occurs like todays release only a day after last release.

--- AlternativeDesktop.py
+++ AlternativeDesktop.py
@@ -43,7 +43,7 @@ from util.logs import setup_logging
 # Only used for pyinstaller to get the dependencies needed for the program. (pyinstaller only looks for explicit imports not load_module)
 from desktop.desktop import main
 
-CURRENT_VERSION = "V0.1.001"
+CURRENT_VERSION = "V0.1.002"
 GITHUB_REPO = "gillsb/Alternative-Desktop"
 RELEASES_URL = f"https://api.github.com/repos/{GITHUB_REPO}/releases/latest"
 SETTINGS_FILE = None
