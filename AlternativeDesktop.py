d5e3f0a78a817d3c3b2b603931d5e2cf5bdca46f
gillsB <78826213+gillsB@users.noreply.github.com>
2024-10-25 19:53:00 -0700

    V0.3.000
    Greatly improved video background playback.
    - Completely redesigned desktop icon storage format for better performance (Dictionary instead of 2d array).
    - Caching icons to prevent endlessly redrawing (Would lag the video background player).
    - Fixed multiple bugs causing endless redrawing (bugs which would endlessly redraw even cached icons).
    - Video Background now seamlessly loops (Fixed a problem where end of video would remove it from the scene and take ~0-1 seconds before it would be reloaded at the start).
    
    Other Improvements:
    - Icons now maintain their aspect ratio. (Instead of being forced into a square aspect ratio icon).
    - Pausing video playback on settings/ right click menu.
    - Default (blank) icons now have their own right click menu (just has edit rather than reusing the full context menu for a set icon).
    - Desktop Icons now show tooltip of full icon Name on hovering the Icon name text area.
    - Improved logging to better detect endless redrawing.
    - Improved logging to better log full errors for debugging.
    - Some smaller errors no longer instantly crash the program.
    
    Bug fixes:
    - Fixed a bug where setting Icon Name Color would not show the updated color selected.

--- AlternativeDesktop.py
+++ AlternativeDesktop.py
@@ -53,7 +53,7 @@ from util.logs import setup_logging
 # Only used for pyinstaller to get the dependencies needed for the program. (pyinstaller only looks for explicit imports not load_module)
 from desktop.desktop import main
 
-CURRENT_VERSION = "V0.2.001"
+CURRENT_VERSION = "V0.3.000"
 GITHUB_REPO = "gillsb/Alternative-Desktop"
 RELEASES_URL = f"https://api.github.com/repos/{GITHUB_REPO}/releases/latest"
 SETTINGS_FILE = None
